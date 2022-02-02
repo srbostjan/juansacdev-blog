d3afa2d86e8c685331c4fb412931765e49efd87e
bostjan <bostjanrivera@gmail.com>
2022-02-02 14:33:00 -0500

    Base project

---
+++ src/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hello World!!</h1>
+      <div>Soy un div</div>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/routes/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Layout from '../containers/Layout';
+import Home from '../containers/Home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
